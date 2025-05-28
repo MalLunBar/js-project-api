b11e5994d64ddb19860d71eca5212a90fd3975ad
MalLunBar <malinelundgren1991@gmail.com>
2025-05-28 13:48:30 +0200

    creating endpoint to find specific thought

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import cors from "cors"
 import express from "express"
+import listEndpoints from "express-list-endpoints"
+import thoughtsData from "./data.json"
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -13,8 +15,29 @@ app.use(express.json())
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!")
+  const endpoints = listEndpoints(app)
+  res.json({
+    message: "Welcome to the Happy Thoughts API",
+    endpoints: endpoints
+  })
 })
+//endpoint for getting all thoughts
+app.get("/thoughts", (req, res) => {
+  res.json(thoughtsData)
+})
+
+//endpoint for getting one thought
+app.get("/thoughts/:id", (req, res) => {
+  console.log(typeof req.params.id)
+  const thought = thoughtsData.find(thought => thought._id === req.params.id)
+
+  if (!thought) {
+    return res.status(404).json({ error: "Thought not found" })
+  }
+  res.json(thought)
+})
+
+
 
 // Start the server
 app.listen(port, () => {
