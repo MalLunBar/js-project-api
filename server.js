53f890dcad4fa652b9c1d83ec0fbfc81082ef6ac
MalLunBar <malinelundgren1991@gmail.com>
2025-06-05 13:07:59 +0200

    add a delete thought endpoint

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ import listEndpoints from "express-list-endpoints"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 
-import thoughtsData from "./data.json"
+// import thoughtsData from "./data.json"
 
-dotenv.config() 
+dotenv.config()
 
-//variable to not modify the original data
-let thoughts = [...thoughtsData]
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/thoughts"
+mongoose.connect(mongoUrl)
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -21,10 +21,29 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/thoughts"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
-//Fortsätt här och ändra local host namn sen 
+
+
+const thoughtSchema = new mongoose.Schema({
+  _id: String,
+  message: String,
+  hearts: Number,
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+})
+//Här lägg
+const Thought = mongoose.model("Thought", thoughtSchema)
+
+// if (process.env.RESET_DB) {
+//   const seedDatabase = async () => {
+//     await Thought.deleteMany({})
+//     thoughtsData.forEach(thought => {
+//       new Thought(thought).save()
+//     })
+//   }
+//   seedDatabase()
+// }
 
 
 // Start defining your routes here
@@ -38,44 +57,139 @@ app.get("/", (req, res) => {
 
 
 
-//endpoint for getting all thoughts
-app.get("/thoughts", (req, res) => {
+//endpoint for getting all thoughts 
+app.get("/thoughts", async (req, res) => {
+  const { likes, minLikes } = req.query
 
-  let filteredThoughts = thoughts
+  const query = {}
 
-  //show hearts with more than 0 likes
-  if (req.query.minHearts !== undefined) {
-    const minHearts = +req.query.minHearts
-    filteredThoughts = filteredThoughts.filter(thought => thought.hearts > minHearts)
+  //Make sure there is error handelning when parameter is not a number 
+
+  if (likes !== undefined) {
+    const numLikes = +likes
+    if (isNaN(numLikes)) {
+      return res.status(400).json({ error: "Query parameter 'likes' must be a number." })
+    }
+    query.hearts = numLikes
   }
 
-  res.json(filteredThoughts)
+  if (minLikes !== undefined) {
+    const numMinLikes = +minLikes
+    if (isNaN(numMinLikes)) {
+      return res.status(400).json({ error: "Query parameter 'likes' must be a number." })
+    }
+    query.hearts = { $gte: numMinLikes }
+  }
+
+  try {
+    const filteredThoughts = await Thought.find(query)
+
+    if (filteredThoughts.length === 0) {
+      return res.status(404).json({
+        success: false,
+        response: [],
+        message: "No thougth found for that query. Please try another one"
+      })
+    }
+    res.status(200).json({
+      success: true,
+      response: filteredThoughts
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      response: error,
+      message: "Server error! Failed to fetch thoughts."
+    })
+  }
 })
 
-//endpoint for getting one thought
-app.get("/thoughts/:id", (req, res) => {
+// endpoint for getting one thought
 
-  const thought = thoughts.find(thought => thought._id === req.params.id)
+app.get("/thoughts/:id", async (req, res) => {
+  const { id } = req.params
 
-  if (!thought) {
-    return res.status(404).json({ error: "Thought not found" })
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({
+      success: false,
+      response: [],
+      message: "Invalid id"
+    })
   }
-  res.json(thought)
+
+  try {
+    const thought = await Thought.findById(id)
+
+    if (!thought) {
+      return res.status(404).json({
+        success: false,
+        response: [],
+        message: "No thougth found"
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      response: thought
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      response: error,
+      message: "Server error! Failed to fetch thoughts."
+    })
+  }
 })
 
 
 //endpoint for deleting a thought
-//returns -1 if thought doesnt exist
-app.delete("/thoughts/:id", (req, res) => {
-  const index = thoughts.findIndex(thought => thought._id === req.params.id)
 
-  if (index == -1) {
-    return res.status(404).json({ error: "thought dosnt exist" })
+app.delete("/thoughts/:id", async (req, res) => {
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({
+      success: false,
+      response: [],
+      message: "Could not delete! Invalid id"
+    })
   }
 
-  const deletedThought = thoughts.splice(index, 1)[0]
-  res.json({ message: "Thought deleted", deletedThought })
+  try {
+    const thought = await Thought.findByIdAndDelete(id)
+
+    if (!thought) {
+      return res.status(404).json({
+        success: false,
+        response: [],
+        message: "No thougth found"
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      response: thought,
+      message: "Was successfully deleted"
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      response: error,
+      message: "Server error! Failed to fetch thoughts."
+    })
+  }
 })
+
+// app.delete("/thoughts/:id", (req, res) => {
+//   const index = thoughts.findIndex(thought => thought._id === req.params.id)
+
+//   if (index == -1) {
+//     return res.status(404).json({ error: "thought dosnt exist" })
+//   }
+
+//   const deletedThought = thoughts.splice(index, 1)[0]
+//   res.json({ message: "Thought deleted", deletedThought })
+// })
 
 
 // Start the server
